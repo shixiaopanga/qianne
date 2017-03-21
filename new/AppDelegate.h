adb94e909d79efd00127beeab1adf44f22ed0fde
lzd <626227182@qq.com>
2017-03-21 17:05:55 +0800

    Initial Commit

---
+++ new/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  new
+//
+//  Created by lzd on 2017/3/21.
+//  Copyright © 2017年 lzd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
